6097699d8b3a3403c91cb335dbfc1011e72470cf
MacAir <hackingexplained@gmail.com>
2023-08-13 02:01:03 +0100

    project init and mongodb config

---
+++ src/main/java/com/becoder/becoder/BecoderApplication.java
@@ -0,0 +1,13 @@
+package com.becoder.becoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BecoderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BecoderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/becoder/becoder/BecoderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.becoder.becoder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BecoderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
